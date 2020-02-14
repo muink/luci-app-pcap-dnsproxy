b4323e6ff5fa2bac2df72f40174fc3db21d81272
muink <hukk1996@gmail.com>
2020-02-14 13:42:09 +0800

    uci2cfg-tool-1

---
+++ root/usr/bin/pcap-dnsproxy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Copyright (C) 2020 muink <https://github.com/muink>
+# This is free software, licensed under the Apache License, Version 2.0
+
+. /lib/functions.sh
+
+CONFDIR=/etc/pcap-dnsproxy
+RAWCONFIGFILE=$CONFDIR/Config.conf-opkg
+CONFIGFILE=$CONFDIR/Config.conf
+RAWHOSTSFILE=$CONFDIR/Hosts.conf-opkg
+HOSTSFILE=$CONFDIR/Hosts.conf
+RAWIPFILTERFILE=$CONFDIR/IPFilter.conf-opkg
+IPFILTERFILE=$CONFDIR/IPFilter.conf
+
+config_load "pcap-dnsproxy"
