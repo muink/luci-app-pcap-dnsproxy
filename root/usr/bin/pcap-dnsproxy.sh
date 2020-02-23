64897334bcb6115ad8fc5e7e4e684327e607b446
muink <hukk1996@gmail.com>
2020-02-23 12:53:17 +0800

    base

--- root/usr/bin/pcap-dnsproxy.sh
+++ root/usr/bin/pcap-dnsproxy.sh
@@ -13,3 +13,35 @@ RAWIPFILTERFILE=$CONFDIR/IPFilter.conf-opkg
 IPFILTERFILE=$CONFDIR/IPFilter.conf
 
 config_load "pcap-dnsproxy"
+
+
+
+
+base_set() {
+	local section="$1"
+	# Base
+	local variable_list="\
+	 cfg_ver\
+	 cfg_refsh_time\
+	 large_buff_size\
+	 additional_path\
+	 hosts_cfg_name\
+	 ipfilter_file_name\
+	"
+	for _var in $variable_list; do local $_var; done
+	for _var in $variable_list; do config_get $_ver "$section" $_ver; done
+
+
+# Base
+local command
+if [ "$cfg_ver" != "" ];            then command="$command s@^\(Version\) =.*\$@\1 = ${cfg_ver}@;" ; fi
+if [ "$cfg_refsh_time" != "" ];     then command="$command s@^\(File Refresh Time\) =.*\$@\1 = ${cfg_refsh_time}@;" ; fi
+if [ "$large_buff_size" != "" ];    then command="$command s@^\(Large Buffer Size\) =.*\$@\1 = ${large_buff_size}@;" ; fi
+if [ "$additional_path" != "" ];    then command="$command s@^\(Additional Path\) =.*\$@\1 = ${additional_path}@;" ; fi
+if [ "$hosts_cfg_name" != "" ];     then command="$command s@^\(Hosts File Name\) =.*\$@\1 = ${hosts_cfg_name}@;" ; fi
+if [ "$ipfilter_file_name" != "" ]; then command="$command s@^\(IPFilter File Name\) =.*\$@\1 = ${ipfilter_file_name}@;" ; fi
+
+	sed -i "1,/^\[.*\]$/ { $command }" $CONFIGFILE
+
+}
+
