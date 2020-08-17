b8a2e13d0fea978d1dcd48d663e5355591ec36fd
muink <hukk1996@gmail.com>
2020-08-18 00:21:59 +0800

    rules auto

--- luasrc/model/cbi/pcap-dnsproxy/rules.lua
+++ luasrc/model/cbi/pcap-dnsproxy/rules.lua
@@ -8,7 +8,7 @@ local sys	= require("luci.sys")
 local util	= require("luci.util")
 local packageName = "pcap-dnsproxy"
 local conf = packageName
-local config = "/etc/config/" .. conf
+local config = "/etc/config/" .. packageName
 local whiteconf = "/etc/pcap-dnsproxy/WhiteList.txt"
 local routingconf = "/etc/pcap-dnsproxy/Routing.txt"
 local dnscryptconf = "/etc/pcap-dnsproxy/dnscrypt-resolvers.csv"
@@ -27,6 +27,7 @@ end
 
 
 m = Map(conf, "")
+m.pageaction = false
 
 
 local WhiteTime = tostring(util.trim(sys.exec("cat " .. whiteconf .. " | sed -n '4p' | cut -f2 -d: | sed -n '/[0-9]\\+-[0-9]\\+-[0-9]\\+/ p'")))
@@ -39,7 +40,102 @@ local alt_white_url = tostring(util.trim(sys.exec("uci get pcap-dnsproxy.@" .. c
 local routing_url = tostring(util.trim(sys.exec("uci get pcap-dnsproxy.@" .. conf .. "[-1].routing_url 2>/dev/null")))
 local routing_v6_url = tostring(util.trim(sys.exec("uci get pcap-dnsproxy.@" .. conf .. "[-1].routing_v6_url 2>/dev/null")))
 
-w = m:section(TypedSection, "pcap-dnsproxy", whitelist)
+
+a = m:section(TypedSection, "rule", nil)
+a.anonymous = true
+
+aauto = a:option(ListValue, "aauto", translate("Auto Update"))
+aauto:value("0", translate("Disable"))
+aauto:value("1", translate("General"))
+aauto:value("2", translate("Crontab"))
+aauto.rmempty = false
+
+aauto_white = a:option(Flag, "aauto_white", translate("Update ") .. whitelist)
+aauto_white.rmempty = true
+aauto_white:depends("aauto", "1")
+aauto_white:depends("aauto", "2")
+
+aauto_white_src = a:option(ListValue, "aauto_white_src", translate("Update Source"))
+aauto_white_src:value("main", translate("Main Data source"))
+aauto_white_src:value("alt", translate("Alternate Data source"))
+aauto_white_src.rmempty = true
+aauto_white_src:depends("aauto_white", "1")
+
+aauto_route = a:option(Flag, "aauto_route", translate("Update ") .. routinglist)
+aauto_route.rmempty = true
+aauto_route:depends("aauto", "1")
+aauto_route:depends("aauto", "2")
+
+aauto_cron = a:option(Value, "aauto_cron", translate("Update Schedule"))
+aauto_cron.placeholder = "0 9 * * 3"
+aauto_cron.rmempty = true
+aauto_cron:depends("aauto", "2")
+
+aauto_cycle = a:option(ListValue, "aauto_cycle", translate("Update Cycle"))
+aauto_cycle:value("week", translate("Week"))
+aauto_cycle:value("month", translate("Month"))
+aauto_cycle.rmempty = true
+aauto_cycle:depends("aauto", "1")
+
+aauto_week = a:option(ListValue, "aauto_week", translate("Update Date"))
+aauto_week:value("0", translate("Sunday"))
+aauto_week:value("1", translate("Monday"))
+aauto_week:value("2", translate("Tuesday"))
+aauto_week:value("3", translate("Wednesday"))
+aauto_week:value("4", translate("Thursday"))
+aauto_week:value("5", translate("Friday"))
+aauto_week:value("6", translate("Saturday"))
+aauto_week.rmempty = true
+aauto_week:depends("aauto_cycle", "week")
+
+aauto_month = a:option(ListValue, "aauto_month", translate("Update Date"))
+aauto_month:value("1", translate("Day 1"))
+aauto_month:value("7", translate("Day 7"))
+aauto_month:value("14", translate("Day 14"))
+aauto_month:value("21", translate("Day 21"))
+aauto_month:value("28", translate("Day 28"))
+aauto_month.rmempty = true
+aauto_month:depends("aauto_cycle", "month")
+
+aauto_time = a:option(ListValue, "aauto_time", translate("Update Time"))
+aauto_time:value("0", "0:00")
+aauto_time:value("1", "1:00")
+aauto_time:value("2", "2:00")
+aauto_time:value("3", "3:00")
+aauto_time:value("4", "4:00")
+aauto_time:value("5", "5:00")
+aauto_time:value("6", "6:00")
+aauto_time:value("7", "7:00")
+aauto_time:value("8", "8:00")
+aauto_time:value("9", "9:00")
+aauto_time:value("10", "10:00")
+aauto_time:value("11", "11:00")
+aauto_time:value("12", "12:00")
+aauto_time:value("13", "13:00")
+aauto_time:value("14", "14:00")
+aauto_time:value("15", "15:00")
+aauto_time:value("16", "16:00")
+aauto_time:value("17", "17:00")
+aauto_time:value("18", "18:00")
+aauto_time:value("19", "19:00")
+aauto_time:value("20", "20:00")
+aauto_time:value("21", "21:00")
+aauto_time:value("22", "22:00")
+aauto_time:value("23", "23:00")
+aauto_time.rmempty = true
+aauto_time:depends("aauto", "1")
+
+aauto_apply = a:option(Button, "_aauto_apply", translate("Save & Apply"))
+aauto_apply.inputtitle = translate("Save & Apply")
+aauto_apply.inputstyle = "apply"
+aauto_apply.write = function()
+    --m.uci:save(conf)
+    m.uci:commit(conf)
+    m.uci:apply()
+end
+
+
+w = m:section(TypedSection, "rule", whitelist)
 w.anonymous = true
 
 wstate = w:option(DummyValue, "_wstate", translate("Last update"))
@@ -85,7 +181,12 @@ function waltup.write (self, section)
 	end
 end
 
---add to scheduled tasks
+wsave = w:option(Button, "_wsave", translate("Save & Apply"))
+wsave.inputtitle = translate("Save & Apply")
+wsave.inputstyle = "apply"
+wsave.write = function()
+    m.uci:apply()
+end
 
 
 local RoutingTime = tostring(util.trim(sys.exec("cat " .. routingconf .. " | sed -n '5p' | cut -f2 -d: | sed -n '/[0-9]\\+-[0-9]\\+-[0-9]\\+/ p'")))
@@ -94,7 +195,7 @@ if not RoutingTime or RoutingTime == "" then RoutingTime = translate("None"); el
 	.. "    " .. "IPv6: " .. tostring(util.trim(sys.exec("echo $(( $(sed -n '/^## IPv6/,/^## .*/ p' '" .. routingconf .. "' | grep '[^[:space:]]' | grep -Ev '#|^\\\[' | sed -n '$=') + 0 ))"))) .. translate(" Rules");
 end
 
-r = m:section(TypedSection, "pcap-dnsproxy", routinglist)
+r = m:section(TypedSection, "rule", routinglist)
 r.anonymous = true
 
 rstate = r:option(DummyValue, "_rstate", translate("Last update"))
@@ -131,8 +232,15 @@ function rup.write (self, section)
 	end
 end
 
+rsave = r:option(Button, "_rsave", translate("Save & Apply"))
+rsave.inputtitle = translate("Save & Apply")
+rsave.inputstyle = "apply"
+rsave.write = function()
+    m.uci:apply()
+end
 
-d = m:section(TypedSection, "pcap-dnsproxy", dnscryptdb)
+
+d = m:section(TypedSection, "rule", dnscryptdb)
 d.anonymous = true
 
 dstate = d:option(DummyValue, "_dstate", translate("Last update"))
@@ -145,5 +253,6 @@ dup.inputstyle = "apply"
 function dup.write (self, section)
 	sys.call ("curl -Lo '" .. dnscryptconf .. "' 'https://raw.githubusercontent.com/dyne/dnscrypt-proxy/master/dnscrypt-resolvers.csv'")
 end
+
 
 return m
